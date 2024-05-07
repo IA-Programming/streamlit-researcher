aca5bf23e436d9eaeaaac00d8f4e009bcdc3a256
AI_Programming <118508502+IA-Programming@users.noreply.github.com>
2024-05-07 01:13:20 +0000

    deploy

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Real-time Google Research Assistant
 
-This Streamlit application acts as a real-time research assistant, leveraging the power of Google search and OpenAI's GPT-4 model to generate responses to user queries based on content scraped from the first 10 pages of Google search results.
+This Streamlit application acts as a real-time research assistant, leveraging the power of Google search and Groq models to generate responses to user queries based on content scraped from the first 10 pages of Google search results.
 
 ## Features
 
 - **Search and Scrape:** Automates the process of searching Google with a user's query, scraping the first 10 pages of results.
 - **In-Memory Vector Database:** Uses the scraped content to create an in-memory vector database for efficient information retrieval.
-- **OpenAI GPT-4 Integration:** Utilizes OpenAI's GPT-4 model to generate a detailed response to the research question based on the most relevant snippets from the scraped content.
 
 ## Installation
 
@@ -20,15 +19,15 @@ Before running the application, you need to install the necessary dependencies. 
 
 ### Setting Up Your API Key
 
-- An OpenAI API key is necessary for running the application. You can get your API key from [OpenAI Platform](https://platform.openai.com/api-keys) if you have a ChatGPT Plus account.
+- An Groq API key is necessary for running the application. You can get your API key from [Groq Platform](https://console.groq.com/keys) if you have a Groq account.
 
 ## Usage
 
 1. **Start the Streamlit Application:**
 Run the application by navigating to the application's directory and using the command:
 
-2. **Enter Your OpenAI API Key:**
-Upon launching the application, you'll be prompted to enter your OpenAI API key in the sidebar.
+2. **Enter Your Groq API Key:**
+Upon launching the application, you'll be prompted to enter your Groq API key in the sidebar.
 
 3. **Using the Application:**
 - Enter your research question in the provided text input.
@@ -45,6 +44,13 @@ Upon launching the application, you'll be prompted to enter your OpenAI API key 
 
 Contributions to improve the Real-time Google Research Assistant are welcome. Please feel free to fork the repository, make changes, and submit pull requests.
 
+## Suppport Us
+
+<a href="https://www.buymeacoffee.com/blazzmocompany"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=blazzmocompany&button_colour=40DCA5&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00"></a>
+
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/blazzmocompany)
+
 ## License
 
-This project is open-sourced under the MIT License. See the LICENSE file for more details.
+This project is open-sourced under the MIT License. See the [LICENSE](./LICENSE) file for more details.
--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,3 @@
-
-# pip install pydantic==1.8.2
-# pip install langchain langchain-community langchain-openai langchain-core pydantic==1.10.2 docarray==0.32.1
-# conda create -n streamlit python=3.11.4
-
 import os
 
 # Importing necessary modules and classes
@@ -11,7 +6,7 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import DocArrayInMemorySearch
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 from langchain.prompts import ChatPromptTemplate
-from langchain_openai.chat_models import ChatOpenAI
+from langchain_groq import ChatGroq
 from langchain_core.output_parsers import StrOutputParser
 import logging
 import datetime
@@ -25,26 +20,28 @@ os.system('playwright install')
 os.system('playwright install-deps')
 
 TOP_MATCHES = 5
-GPT_MODEL = 'gpt-4-1106-preview' # 'gpt-3.5-turbo'# Defining the model to be used with OpenAI
+GPT_MODEL = 'mixtral-8x7b-32768' # 'gpt-3.5-turbo'# Defining the model to be used with OpenAI
 TEXT_CHUNK_SIZE = 1000
 TEXT_CHUNK_OVERLAP = 100
 
 st.sidebar.markdown("""
-An OpenAI API key is required to make the client-sided call to the LLM.  A ChatGPT Plus account is required for this service.   
+An Groq API key is required to make the client-sided call to the LLM.  A Groq account is required for this service.   
 """)
 
-
+models = {"LLaMA3 8b": "llama3-8b-8192","LLaMA3 70b": "llama3-70b-8192","Mixtral 8x7b": "mixtral-8x7b-32768","Gemma 7b": "gemma-7b-it"}
 API_KEY = st.sidebar.text_input("API key: ", key="api_key")
+MODELO = st.sidebar.selectbox("Select you model", options=list(models.keys()), index=0)
+GPT_MODEL = models[MODELO]
 
 st.sidebar.markdown("""
-If you have a ChatGPT Plus account, you can get one of your api keys from here:                 
-https://platform.openai.com/api-keys    
+If you have a ChatGroq account, you can get one of your api keys from here:                 
+https://console.groq.com/keys   
 """)
 
 
 # Setting the OpenAI API key environment variable
 if API_KEY:
-    os.environ['OPENAI_API_KEY'] = API_KEY
+    os.environ['GROQ_API_KEY'] = API_KEY
 
 # Explicitly disable parallelism in tokenizers to avoid the multiprocessing issue.
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -57,8 +54,9 @@ logging.getLogger('transformers').setLevel(logging.WARNING)
 logging.getLogger('sentence_transformers').setLevel(logging.WARNING)
 
 embedding = HuggingFaceEmbeddings(
-    model_name='sentence-transformers/all-MiniLM-L6-v2',
-    model_kwargs={'device': 'cpu'}
+    model_name='BAAI/bge-small-en-v1.5',
+    model_kwargs={'device': 'cpu'},
+    cache_folder="./"
 )
 
 template = """
@@ -79,19 +77,18 @@ template = """
 """
 
 st.title("Real-time Google Research Assistant")
-st.subheader("built by contractorwolf")
+st.subheader("built by AI-Programming")
 st.markdown("""
-    This application takes a question from the user and does a google search query on that question.  Its then scrapes the 
+    This application takes a question from the user and does a google search query on that question.  Then scrapes the 
     first 10 pages from the search then scrapes the content from each of the pages.  That content is loaded in a vector
     db (in memory) and the question is posed to the vector db to find the top 5 most relevant snippets.  Those snippets 
-    are sent to the OpenAI GPT-4 model to generate a response to the initial research question before displaying the 
+    are sent to the Groq models to generate a response to the initial research question before displaying the 
     response to the user.  
     
     Make sure you wait at least 30 seconds for the scraping and processing to complete.
 """)
 
 if 'search_logs' not in st.session_state:
-    
     st.session_state['search_logs'] = []
 if 'search_result' not in st.session_state:
     st.session_state['search_result'] = ''
@@ -122,7 +119,7 @@ async def main_async(question, st):
     prompt = ChatPromptTemplate.from_template(template)
 
     # Initializing the chat model with the OpenAI API key and model choice
-    model = ChatOpenAI(openai_api_key=API_KEY, model=GPT_MODEL)
+    model = ChatGroq(groq_api_key=API_KEY, model=GPT_MODEL)
 
     # Setting up a parser to handle the output from the chat model
     parser = StrOutputParser()
@@ -170,6 +167,7 @@ if submitted:
         trigger_search(query, st)    
 
 if st.session_state['search_result']:
+    st.markdown()
     st.text_area("Search Results", value=st.session_state['search_result'], height=300)
 
 if st.session_state['search_logs']:
